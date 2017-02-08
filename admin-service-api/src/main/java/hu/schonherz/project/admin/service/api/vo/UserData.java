6a6ecd8460f5628f677eef7297cd7c5f7e2bd91b
Joey <kovtamas1991@gmail.com>
2017-02-08 17:49:12 +0100

    Removed all dependencies from admin-service-api, added boiler-plate code.

--- admin-service-api/src/main/java/hu/schonherz/project/admin/service/api/vo/UserData.java
+++ admin-service-api/src/main/java/hu/schonherz/project/admin/service/api/vo/UserData.java
@@ -1,11 +1,8 @@
 package hu.schonherz.project.admin.service.api.vo;
 
 import java.io.Serializable;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.util.Objects;
 
-@Data
-@NoArgsConstructor
 public class UserData implements Serializable {
 
     private static final long serialVersionUID = 10L;
@@ -19,5 +16,91 @@ public class UserData implements Serializable {
     private String password;
 
     private UserRole userRole;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(UserRole userRole) {
+        this.userRole = userRole;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 29 * hash + Objects.hashCode(this.id);
+        hash = 29 * hash + Objects.hashCode(this.email);
+        hash = 29 * hash + Objects.hashCode(this.username);
+        hash = 29 * hash + Objects.hashCode(this.password);
+        hash = 29 * hash + Objects.hashCode(this.userRole);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final UserData other = (UserData) obj;
+        if (!Objects.equals(this.email, other.email)) {
+            return false;
+        }
+        if (!Objects.equals(this.username, other.username)) {
+            return false;
+        }
+        if (!Objects.equals(this.password, other.password)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        if (this.userRole != other.userRole) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "UserData{" + "id=" + id + ", email=" + email + ", username=" + username + ", password=" + password + ", userRole=" + userRole + '}';
+    }
 
 }
