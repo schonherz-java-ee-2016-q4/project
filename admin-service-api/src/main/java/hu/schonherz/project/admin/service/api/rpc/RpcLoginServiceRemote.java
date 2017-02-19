4965ad13747be5a4e0590912c322d95570fa3f41
aron9609 <aron9609@gmail.com>
2017-02-19 19:14:08 +0100

    remove

--- admin-service-api/src/main/java/hu/schonherz/project/admin/service/api/rpc/RpcLoginServiceRemote.java
+++
@@ -1,9 +0,0 @@
-package hu.schonherz.project.admin.service.api.rpc;
-
-import hu.schonherz.project.admin.service.api.vo.UserData;
-
-public interface RpcLoginServiceRemote {
-
-    UserData rpcLogin(String username, String plainTextPassword) throws FailedRpcLoginAttemptException;
-
-}
